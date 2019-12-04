06bcb45b51a5f4c80472094f89693c318ca2e20d
Ayush Gupta <aa.97gupta@gmail.com>
2019-12-04 16:21:26 +0530

    FIX: tag animation

--- src/animation/DisperseWord/index.js
+++ src/animation/DisperseWord/index.js
@@ -5,75 +5,96 @@ class DisperseWord extends Component {
     render() {
         const tag = ["G", "e", "ɘ", "k", "y", "A", "n", "t", "s"];
         let transitionState = [];
-        transitionState.push([
+        const rotateG = Math.random() > 0.5 ? Math.random() * 120 : Math.random() * -120;
+        transitionState.push([{
+            start: 'self',
+            duration: 600,
+            easing: "easeInOut",
+            properties: [{
+                startValue: 0,
+                endValue: rotateG,
+                property: 'rotate',
+            },
             {
-                start: 'self',
-                duration: 500,
-                easing: "easeInOut",
-                properties: [
-                    {
-                        startValue: 0,
-                        endValue: Math.random() > 0.5 ? Math.random() * 120 : Math.random() * -120,
-                        property: 'rotate',
-                    },
-                    {
-                        startValue: 1,
-                        endValue: 7,
-                        property: 'scale',
-                    },
-                    {
-                        startValue: 0,
-                        endValue: -650,
-                        property: 'translateX',
-                        unit: '%',
-                    },
-                    {
-                        startValue: 0,
-                        endValue: 530,
-                        property: 'translateY',
-                        unit: '%',
-                    },
-                ],
+                startValue: 1,
+                endValue: 7,
+                property: 'scale',
+            },
+            {
+                startValue: 0,
+                endValue: -650,
+                property: 'translateX',
+                unit: '%',
+            },
+            {
+                startValue: 0,
+                endValue: 530,
+                property: 'translateY',
+                unit: '%',
+            }],
+        },
+        {
+            start: 600,
+            duration: 400,
+            easing: "easeInOut",
+            properties: [{
+                startValue: rotateG,
+                endValue: rotateG + (Math.random() > 0.5 ? Math.random() * 120 : Math.random() * -120),
+                property: 'rotate',
+            },
+            {
+                startValue: 7,
+                endValue: 4,
+                property: 'scale',
+            },
+            {
+                startValue: -650,
+                endValue: -1250,
+                property: 'translateX',
+                unit: '%',
+            },
+            {
+                startValue: 530,
+                endValue: 930,
+                property: 'translateY',
+                unit: '%',
             }
-        ])
+            ],
+        }])
         tag.map(() => {
             const rotationFactor = Math.random() > 0.5 ? 180 : -180;
             console.log(Math.random())
             const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
             const xFactor = (Math.random() * 8000 * plusOrMinus) / 2;
             const yFactor = (Math.random() * 1500 * plusOrMinus) / 2;
-            transitionState.push([
+            transitionState.push([{
+                start: 'self',
+                duration: 600,
+                name: 'first',
+                easing: "easeInOut",
+                properties: [{
+                    startValue: 0,
+                    endValue: Math.random() * rotationFactor,
+                    property: 'rotate',
+                },
                 {
-                    start: 'self',
-                    duration: 500,
-                    name: 'first',
-                    easing: "easeInOut",
-                    properties: [
-                        {
-                            startValue: 0,
-                            endValue: Math.random() * rotationFactor,
-                            property: 'rotate',
-                        },
-                        {
-                            startValue: 1,
-                            endValue: 7,
-                            property: 'scale',
-                        },
-                        {
-                            startValue: 0,
-                            endValue: xFactor,
-                            property: 'translateX',
-                            unit: '%',
-                        },
-                        {
-                            startValue: 0,
-                            endValue: yFactor,
-                            property: 'translateY',
-                            unit: '%',
-                        },
-                    ],
-                }
-            ])
+                    startValue: 1,
+                    endValue: 7,
+                    property: 'scale',
+                },
+                {
+                    startValue: 0,
+                    endValue: xFactor,
+                    property: 'translateX',
+                    unit: '%',
+                },
+                {
+                    startValue: 0,
+                    endValue: yFactor,
+                    property: 'translateY',
+                    unit: '%',
+                }],
+            }])
             return true;
         })
 
@@ -83,8 +104,7 @@ class DisperseWord extends Component {
                     tagName='h1'
                     className={item === "G" ? "disperse-tagG" : 'disperse-tag'}
                     parallaxData={transitionState[index]}
-                >
-                    <div className="disperse-tag">{item}</div>
+                >{item}
                 </Plx>
                 )}
             </div>
--- src/animation/VideoBG/index.js
+++ src/animation/VideoBG/index.js
@@ -1,14 +1,46 @@
+import Plx from "react-plx";
 import React, { Component } from "react";
 
 import videoURL from "../../assets/Images/home_video.mp4";
 
 class VideoBG extends Component {
     render() {
+        const transitionState = [{
+            start: 0,
+            duration: 1000,
+            easing: "easeInOut",
+            properties: [{
+                startValue: 0,
+                endValue: 5,
+                property: 'rotate',
+            },
+            {
+                startValue: 1.1,
+                endValue: 2,
+                property: 'scale',
+            },
+            {
+                startValue: 2,
+                endValue: 5,
+                property: 'blur',
+            },
+            {
+                startValue: 0.4,
+                endValue: 0.4,
+                property: 'brightness',
+            },
+            ],
+        }]
         return (
-            <video className="bg-video" loop autoPlay muted>
+            <Plx
+                tagName="video"
+                className="bg-video"
+                parallaxData={transitionState}
+                loop autoPlay muted
+            >
                 <source src={videoURL} type="video/mp4" />
                 <source src={videoURL} type="video/ogg" />
-            </video>
+            </Plx>
         );
     }
 }
