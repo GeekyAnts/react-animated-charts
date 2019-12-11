1a1dc3d24263e374df77e958d834a758ea6e5c2a
Ayush Gupta <aa.97gupta@gmail.com>
2019-12-11 19:23:42 +0530

    GIT

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// serviceWorker.unregister();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-// export { default as HamiltonianGraph } from './ForcedGraph/HamiltonianGraph.js';
-// export { default as ForceDirectedGraph } from './ForcedGraph/ForceDirectedGraph.js';
-// export { default as BubbleChart } from "./BubbleChart/BubbleChart";
+export { default as HamiltonianGraph } from './ForcedGraph/HamiltonianGraph.js';
+export { default as ForceDirectedGraph } from './ForcedGraph/ForceDirectedGraph.js';
+export { default as BubbleChart } from "./BubbleChart/BubbleChart";
